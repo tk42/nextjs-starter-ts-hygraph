58692f33aceebed52f61d4a01cb31a20fea8f2f4
tk42 <nsplat@gmail.com>
2022-11-27 04:37:47 +0000

    Enabled drop-shadow effect

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # nextjs-starter-ts-hygraph
+
+NextJS starter with Typescript connected to hygraph.
+
+![image](./image.png)
--- app/components/cover-image.tsx
+++ app/components/cover-image.tsx
@@ -13,8 +13,8 @@ const CoverImage = ({ title, src, slug }: Props) => {
     <Image
       src={src}
       alt={`Cover Image for ${title}`}
-      className={cn('shadow-sm w-full', {
-        'hover:shadow-lg transition-shadow duration-200': slug,
+      className={cn('drop-shadow-sm w-full', {
+        'hover:drop-shadow-xl transition-shadow duration-200': slug,
       })}
       width={1300}
       height={630}
